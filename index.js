8d0b6c11e0306c5bedc0bcaff8a365b867548112
Kenneth Kipkoech <kenkipkoech57@gmail.com>
2024-12-15 07:24:37 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Remove the "main" element from the DOM
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+// Create a new "h1" element
+const newHeader = document.createElement("h1");
+
+// Set an id attribute of "victory" to the new header
+newHeader.id = "victory";
+
+// Add content to the new header using textContent
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Append the new header to the body
+document.body.append(newHeader);
